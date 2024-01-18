a48dfe5aaa60d0df522c9511253890aab951ecf2
MorasDeFresa <bencito10@gmail.com>
2024-01-18 18:39:54 -0500

    Fundamentos javascript

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,7 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  return !isNaN(Date.parse(fecha)) && typeof fecha == 'object';
 }
 
 module.exports = esFechaValida;
--- M05 Fundamentos Javascript/ejercicios/27.js
+++ M05 Fundamentos Javascript/ejercicios/27.js
@@ -2,6 +2,9 @@ function combinarNombres(nombre, apellido) {
   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
   // Tu código:
+  // var nuevoString = nombre + " " + apellido
+  var string = `${nombre} ${apellido}`
+  return string
 }
 
 module.exports = combinarNombres;
